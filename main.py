3de79b082806fd63ef7c01d3abe6911e049c6e1c
sadzerolol <90262326+sadzerolol@users.noreply.github.com>
2021-09-07 17:05:03 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from pygame import mixer
 pygame.init()
 
 # create the screen
-screen = pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((1280, 600))
 
 # Background
 background = pygame.image.load('background.png')
